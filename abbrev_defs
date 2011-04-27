b2ecbefe021bdb2bd525355b12da0d2e9b26ab29
crispy <cvale@rocketninja.com>
2011-04-27 15:30:22 -0700

    initial version

---
+++ abbrev_defs
@@ -0,0 +1,93 @@
+;;-*-coding: emacs-mule;-*-
+(define-abbrev-table 'Buffer-menu-mode-abbrev-table '())
+
+(define-abbrev-table 'Custom-mode-abbrev-table '())
+
+(define-abbrev-table 'actionscript-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'awk-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'bsh-script-mode-abbrev-table '())
+
+(define-abbrev-table 'c++-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'c-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'comint-mode-abbrev-table '())
+
+(define-abbrev-table 'completion-list-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-colon-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-javaprop-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-ppd-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-space-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-unix-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-windows-mode-abbrev-table '())
+
+(define-abbrev-table 'conf-xdefaults-mode-abbrev-table '())
+
+(define-abbrev-table 'diff-mode-abbrev-table '())
+
+(define-abbrev-table 'emacs-lisp-mode-abbrev-table '())
+
+(define-abbrev-table 'fundamental-mode-abbrev-table '())
+
+(define-abbrev-table 'global-abbrev-table '())
+
+(define-abbrev-table 'grep-mode-abbrev-table '())
+
+(define-abbrev-table 'groovy-mode-abbrev-table '())
+
+(define-abbrev-table 'idl-mode-abbrev-table '())
+
+(define-abbrev-table 'inferior-emacs-lisp-mode-abbrev-table '())
+
+(define-abbrev-table 'java-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'jde-mode-abbrev-table
+  '(
+    ("catch" "catch" c-electric-continued-statement 0)
+    ("else" "else" c-electric-continued-statement 1)
+    ("finally" "finally" c-electric-continued-statement 0)
+    ("while" "while" c-electric-continued-statement 0)
+   ))
+
+(define-abbrev-table 'jde-run-mode-abbrev-table '())
+
+(define-abbrev-table 'lisp-mode-abbrev-table '())
+
+(define-abbrev-table 'objc-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'pike-mode-abbrev-table
+  '(
+   ))
+
+(define-abbrev-table 'select-tags-table-mode-abbrev-table '())
+
+(define-abbrev-table 'shell-mode-abbrev-table '())
+
+(define-abbrev-table 'special-mode-abbrev-table '())
+
+(define-abbrev-table 'text-mode-abbrev-table '())
+
+(define-abbrev-table 'tpl-mode-abbrev-table '())
+
+(define-abbrev-table 'vc-git-log-view-mode-abbrev-table '())
+
---
+++ init.el
@@ -0,0 +1,116 @@
+;;
+
+(setq magic-mode-alist (cons '("<\\?xml\\s " . nxml-mode) magic-mode-alist))
+(setq auto-mode-alist  (cons '("\\.x?html?$" . html-mode) auto-mode-alist))
+
+(load "~/.emacs.d/nxhtml/autostart.el")
+(load "~/.emacs.d/haskell-mode/haskell-site-file")
+
+(setq inhibit-startup-screen t)
+(put 'narrow-to-region 'disabled nil)
+(scroll-bar-mode nil)
+(tool-bar-mode -1)
+
+(add-to-list 'exec-path "/opt/local/bin")
+(setenv "PATH" (concat "/opt/local/bin:" (getenv "PATH")))
+
+(defun fix-format-buffer ()
+  "indent, untabify and remove trailing whitespace for a buffer"
+  (interactive)
+  (save-excursion 
+    (delete-trailing-whitespace)
+    (intend-region (point-min) (point-max))
+    (untabify (point-min) (point-max))))
+
+(setq special-display-buffer-names '("*compilation*" "*grep*" "*Find*"))
+
+;; the native os x version of emacs reports "ns" as the name of the
+;; windowing system. X on os x (and everywhere else i've tested)
+;; reports x.
+(if (eq window-system 'ns)
+      (custom-set-faces
+       '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundery "apple" :family "Monaco")))))
+      )
+
+(add-to-list 'load-path "/Users/crispywalrus/.emacs.d/local")
+(add-to-list 'load-path "/Users/crispywalrus/.emacs.d/jdee/lisp")
+
+(load-file "/Users/crispywalrus/.emacs.d/cedet/common/cedet.el")
+
+;; extend cc-mode to understand java annotations
+(require 'java-mode-indent-annotations)
+
+;; indenting etc. the google way
+;; (require 'google-c-style)
+
+;; actionscript editing 
+(require 'actionscript-mode)
+;; (require 'ecmascript-mode)
+
+(require 'mustache-mode)
+
+;; programming language hook functions. all dependent packages should
+;; have been loaded before here
+;; (defun crispy-c-mode-common-hook ()
+;;   (google-set-c-style))
+;; (add-hook 'c-mode-common-hook 'crispy-c-mode-common-hook)
+(defun crispy-java-mode-hook ()
+  (progn
+    (c-set-style "bsd")
+    (setq c-basic-offset 4)
+    (c-toggle-auto-state 1)
+    (c-set-offset 'substatement-open 0)
+    (java-mode-indent-annotations-setup)))
+
+(add-hook 'java-mode-hook 'crispy-java-mode-hook)
+
+;; Enable EDE (Project Management) features
+(global-ede-mode 1)
+
+;; Enable EDE for a pre-existing C++ project
+;; (ede-cpp-root-project "NAME" :file "~/myproject/Makefile")
+
+
+;; Enabling Semantic (code-parsing, smart completion) features
+;; Select one of the following:
+
+;; * This enables the database and idle reparse engines
+(semantic-load-enable-minimum-features)
+
+;; * This enables some tools useful for coding, such as summary mode
+;;   imenu support, and the semantic navigator
+(semantic-load-enable-code-helpers)
+
+;; * This enables even more coding tools such as intellisense mode
+;;   decoration mode, and stickyfunc mode (plus regular code helpers)
+;; (semantic-load-enable-gaudy-code-helpers)
+
+;; * This enables the use of Exuberent ctags if you have it installed.
+;;   If you use C++ templates or boost, you should NOT enable it.
+;; (semantic-load-enable-all-exuberent-ctags-support)
+;;   Or, use one of these two types of support.
+;;   Add support for new languges only via ctags.
+;; (semantic-load-enable-primary-exuberent-ctags-support)
+;;   Add support for using ctags as a backup parser.
+;; (semantic-load-enable-secondary-exuberent-ctags-support)
+
+;; Enable SRecode (Template management) minor-mode.
+;; (global-srecode-minor-mode 1)
+
+;; my generic normal setup
+
+(setq-default indent-tabs-mode nil)
+
+(require 'groovy-mode)
+(add-to-list 'auto-mode-alist '("\\.groovy$" . groovy-mode))
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+(setq-default debug-on-error nil)
+
+;; (require 'markdown-mode)
+;; (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
+
