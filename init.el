d226870b0b63337e69ab80da90201c5d3ca92f34
crispy <crispywalrus@gmail.com>
2012-02-20 15:02:51 -0800

    remove ensime submodule install

--- init.el
+++ init.el
@@ -157,14 +157,11 @@
   ;; If there is more than one, they won't work right.
  '(scala-interpreter "/Applications/typesafe-stack/bin/scala")
  '(virtualenv-root "~/Development/crispy/pyEnvs"))
+
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#000000" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundery "apple" :family "Monaco")))))
-
-
-
-
 
