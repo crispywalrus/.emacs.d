471faae14e1ddbdc67b240e5fa0f72c787df2ac3
crispywalrus <chris.vale@creditkarma.com>
2018-08-02 09:58:23 -0700

    cleanup
    now that ocam/merlin/taureg are working and I seem to have come to a
    conclusion about completion systems this needs to be memorialized.

--- packs/ocaml-pack.el
+++ packs/ocaml-pack.el
@@ -1,10 +1,21 @@
 ;; ocalm et. al.
-(use-package merlin)
+;; (use-package merlin)
 (use-package utop)
 (use-package tuareg
   :config
   (add-hook 'tuarag-model-hook (lambda ()
                                  (utop-minor-mode))))
+
+(let ((opam-share (ignore-errors (car (process-lines "opam" "config" "var" "share")))))
+  (when (and opam-share (file-directory-p opam-share))
+    ;; Register Merlin
+    (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+    (autoload 'merlin-mode "merlin" nil t nil)
+    ;; Automatically start it in OCaml buffers
+    (add-hook 'tuareg-mode-hook 'merlin-mode t)
+    (add-hook 'caml-mode-hook 'merlin-mode t)
+    ;; Use opam switch to lookup ocamlmerlin binary
+    (setq merlin-command 'opam)))
 
 ;; (use-package utop)
 (provide 'ocaml-pack)
