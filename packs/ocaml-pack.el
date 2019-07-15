c6e706bc21c5920a70a8a921d5bfdba0b9e48397
crispy <crispywalrus@gmail.com>
2019-07-15 09:08:20 -0700

    omnibus packages update
    + clean up erlang
    + clean up ocaml
    + add tags
    + update and extend org
    + remove github
    + embiggen magit et. al.

--- packs/ocaml-pack.el
+++ packs/ocaml-pack.el
@@ -1,10 +1,15 @@
 ;; ocalm et. al.
 (require 'dune)
+(require 'dune-flymake)
+
 (use-package reason-mode)
 
 (add-hook 'reason-mode-hook (lambda ()
                               (add-hook 'before-save-hook 'refmt-before-save)
                               (merlin-mode)))
+
+(add-to-list 'auto-mode-alist
+             '("\\(?:\\`\\|/\\)dune-project\\(?:\\.inc\\)?\\'" . dune-mode))
 
 (provide 'ocaml-pack)
 
